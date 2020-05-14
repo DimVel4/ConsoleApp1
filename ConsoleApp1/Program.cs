1bccea19b5baddce761805480f35f832a5173a05
dimvel44 <dimvel44@outlook.com>
2020-05-14 22:35:16 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo<int,string> foo = new Foo<int,string>(3);
+            foo.SampleMethod("fffff");
+
+            Console.ReadKey();
+        }
+
+        interface ISample<T,T1>
+        {
+            T Val { get; }
+            T1 Val1 { get; }
+        }
+
+        class Foo<T,T1> : ISample<T,T1>
+        {
+            private T _val;
+            T1 _val1;
+            public T Val => _val;
+            public T1 Val1 => _val1;
+            public Foo(T val)
+            {
+                this._val = val;
+            }
+
+            public void SampleMethod(T1 _val1)
+            {
+                this._val1 = _val1;
+                Console.WriteLine($"{Val}, {Val1}");
+            }
+
+
+        }
+
+    }
+}
